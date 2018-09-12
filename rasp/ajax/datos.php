456f6ca6af1035fac545cd4651f4210065f62314
Antony Brenes <antonybrenescruz@gmail.com>
2018-09-11 23:28:46 -0600

    Verion final

--- rasp/ajax/datos.php
+++ rasp/ajax/datos.php
@@ -62,17 +62,18 @@
 	}
 
 	function serverUptime (){
+		$time = array();
 		$str   = file_get_contents('/proc/uptime');
 		$num   = floatval($str);
-		$secs  = round(fmod($num, 60), 0); 
+		$time[]  = round(fmod($num, 60), 0); 
 		$num = round($num/60, 2);
-		$mins  = $num % 60;
+		$time[]  = $num % 60;
 		$num = round($num/60, 2);
-		$hours = $num % 24;
+		$time[] = $num % 24;
 		$num = round($num/24, 0);
-		$days  = $num;
+		$time[]  = $num;
 
-		return $days." Dias, ".$hours." hr:".$mins." min:".$secs." seg";
+		return $time;
 	}
 
 	function memoryUsage() {
@@ -101,9 +102,11 @@
 		/*if ($load[0] > 0.80) {
 		    header('HTTP/1.1 503 Too busy, try again later');
 		    //die('Server too busy. Please try again later.');
-		}*/
+		}
+		return $load[0]."-".$load[1]."-".$load[2];
+		*/
 
-		return $load[0]." - ".$load[1]." - ".$load[2];
+		return $load;
 	}
 
 	function diskUsage() {
@@ -127,18 +130,21 @@
 		if (function_exists('exec')) {
 			$unique = array();
 			$tmp = array();
-			@exec ("sudo netstat -plan| awk {'print $5'} | cut -d: -f 1 | sort | uniq -c | sort -n", $results);
-			
+			$sum = 0;
+			@exec ("sudo netstat -anp |grep 'ESTABLISHED' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n", $results);
+			//netstat -plan| awk {'print $5'} | cut -d: -f 1 | sort | uniq -c | sort -n
 			foreach ($results as $result) {
 				if (!in_array($result, $unique)) {
 					$tmp = trim($result);
 					$tmp = explode(" ", $tmp);
-					$unique[] = end($tmp);
+					$unique[] = end($tmp)."\t-\t".$tmp[0];
+					$sum += $tmp[0];
 				}
 			}
 
 			$datos['net_conn'] = count($unique);
 			$datos['inf'] = $unique;
+			$datos['sum'] = $sum;
 
 			echo json_encode($datos);
 		}
@@ -148,7 +154,8 @@
 		//PARA UTILIZAR LA CLASE CONEXION CAMBIAR EL NOMBRE A 'conexion.php' Y ASEGURARSE DE HABER CAMBIADO LAS CREDENCIASLES EN LA CLAS
 		include "../class/conexion_work.php";
 		$conexion = new Conexion();
-		$data = "";
+		
+		$data = "<tr><td>PID</td><td>USENAME</td><td>APPNAME</td><td>CLIEND_ADDR</td><td>CLIENT_PORT</td><td>TIME_START</td><td>QUERY</td></tr>";
 
 		$sql = "SELECT COUNT(*) AS cant FROM pg_stat_activity;";
 		$result = pg_query($sql);
@@ -156,13 +163,14 @@
 
 		$datos['bd_conn'] = $result['cant'];
 
-		$sqlInf = "SELECT pid, usename, application_name, client_addr,
-		client_port, backend_start, state, query
+		//PARA EXTRAER EL TIEMPO QUE LLEVA LEVANTADO EL QUERY
+		/*EXTRACT('epoch' FROM NOW() - backend_start) AS dif*/
+		$sqlInf = "SELECT pid AS /*proc*/pid, usename, application_name, client_addr,
+		client_port, backend_start, /*current_*/query AS query
 		FROM pg_stat_activity;";		
-		$result = pg_query($sqlInf);
+		$result = pg_query($sqlInf) or die(pg_last_error());
 		while($row = pg_fetch_array($result)){
-			$data .= $row['pid']." - ".$row['usename']." - ".$row['application_name']." - ".$row['client_addr']." - ".$row['client_port'];
-			$data .= " - ".$row['backend_start']." - ".$row['state']."\n\n".$row['query']."\n\n";
+			$data .= "<tr><td>".$row['pid']."</td><td>".$row['usename']."</td><td>".$row['application_name']."</td><td>".$row['client_addr']."</td><td>".$row['client_port']."</td><td>".$row['backend_start']."</td><td class='query-column'>".$row['query']."</td></tr>";
 		}
 
 		$datos['bd_inf'] = $data;
--- rasp/index.php
+++ rasp/index.php
@@ -3,148 +3,169 @@
   <head>
     <meta charset="utf-8">
     <title>VMON-Be+</title>
-	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
   <body>
-	<table  id="container" class="container">
+	<table class="container">
 		<tbody>
-			<tr align="center">
-				<td class="block frame" align="center">
-		  			<p align="center" class="canvas-title">DISCO - VELOCIDAD ESCRITURA Y LECTURA</p>
-		  			<div align="center" class="container-info-canvas">
-		  				<span id="w_disk_io" class="info-canvas"></span>&emsp;
-		  				<span id="r_disk_io" class="info-canvas"></span>
-		  			</div>
-		  			<div>
-		  				<div class="block medidor-canvas relative" align="center">
-		  					<span id="w_disk_io-top" class="top"></span>
-		  					<span id="w_disk_io-bot" class="bot"></span>
-		  				</div>
-		  				<canvas id="disk_io" class="block"></canvas>
-		  			</div>			
-		  		</td>
-		  		<td class="block frame">
-		  			<p align="center" class="canvas-title">RAM - CONSUMO</p>
-		  			<div align="center" class="container-info-canvas">
-		  				<span id="ram_act" class="info-canvas">&emsp;</span>
-		  			</div>
-		  			<div>
-		  				<div class="block medidor-canvas relative" align="center">
-		  					<span id="ram-top" class="top"></span>
-		  					<span id="ram-bot" class="bot"></span>
-		  				</div>
-		    			<canvas id="ram"></canvas>
-		  			</div>
-		  		</td>
-		  		<td class="block frame">
-		  			<p align="center" class="canvas-title">CPU - RENDIMIENTO</p>
-		    		<div align="center" class="container-info-canvas">
-		  				<span id="cpu_act" class="info-canvas">&emsp;</span>
-		  			</div>
-		  			<div>
-		  				<div class="block medidor-canvas relative" align="center">
-		  					<span id="cpu-top" class="top"></span>
-		  					<span id="cpu-bot" class="bot"></span>
-		  				</div>
-		    			<canvas id="cpu"></canvas>
-		  			</div>
-		  		</td>
-			</tr>
-			<tr  align="center">
-				<td class="block frame">
-		  			<p align="center" class="canvas-title">BD - CONEXIONES</p>
-		  			<div align="center" class="container-info-canvas">
-		  				<span id="bd_act" class="info-canvas">&emsp;</span>
-		  			</div>
-		  			<div>
-		  				<div class="block medidor-canvas relative" align="center">
-		  					<span id="bd-top" class="top"></span>
-		  					<span id="bd-bot" class="bot"></span>
-		  				</div>
-		    			<canvas id="bd_conn"></canvas>
-		    		</div>
-		  		</td>
-		  		<td class="block frame">
-		  			<p align="center" class="canvas-title">RED - CONEXIONES</p>
-		  			<div align="center" class="container-info-canvas">
-		  				<span id="net_act" class="info-canvas">&emsp;</span>
-		  			</div>
-		  			<div>
-		  				<div class="block medidor-canvas relative" align="center">
-		  					<span id="net-top" class="top"></span>
-		  					<span id="net-bot" class="bot"></span>
-		  				</div>
-		    			<canvas id="server_conn"></canvas>
-		    		</div>
-		  		</td>
-		  		<td class="block frame inf-frame">
-		  			<div id="head-inf">
-		  				<p align="center" class="inf-title">INFORMACION GENERAL - ALERTAS</p>
-		  				<div align="center" class="container-info-canvas">
-			  				<span id="tot_alert" class="info-canvas">&emsp;</span>
-			  			</div>
-		  			</div>
-		  			<div id="body-inf">
-		  				<table class="inf-content">
-		  					<tbody>
-		  						<tr>
-		  							<td>Uptime: </td>
-		  							<td class="inf-data"><span id="uptime"></span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>CPU AVERAGE: </td>
-		  							<td class="inf-data"><span id="cpu_load"></span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Uso de memoria APACHE: </td>
-		  							<td class="inf-data"><span id="usage_ram"></span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Uso HDD: </td>
-		  							<td class="inf-data"><span id="usage_hdd"></span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Alertas CPU: </td>
-		  							<td class="inf-data"><span id="alert_cpu">0</span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Alertas RAM: </td>
-		  							<td class="inf-data"><span id="alert_ram">0</span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Maximo conexiones red: </td>
-		  							<td class="inf-data"><span id="max_http">0</span></td>
-		  						</tr>
-		  						<tr>
-		  							<td>Maximo conexiones BD: </td>
-		  							<td class="inf-data"><span id="max_bd">0</span></td>
-		  						</tr>
-		  					</tbody>
-		  				</table>
-		  			</div>
-		  		</td>
+			<tr>
+				<td colspan="3" class="color-brand">VIRTUAL MONITOR - Be+</td>
 			</tr>
 			<tr>
-				<td class="block frame inf-frame left-marging-80">
-					<div>
-		  			<p align="center" class="inf-title left-marging-80">LISTA DE CONEXIONES ACTUALES EN RED</p>
-		  			<div align="center" class="container-info-canvas">
-			  			<textarea name="inf_net_conn" id="inf_net_conn" class="info-canvas data-list"></textarea>
-			 			</div>
+				<td class="section">
+					<div class="block section-title">
+						<div class="title">DISCO - VELOCIDAD ESCRITURA Y LECTURA</div>
+						<div class="section-general-data">
+							<span id="w_disk_io"></span>&emsp;
+		  					<span id="r_disk_io"></span>
+						</div>
+					</div>					
+					<div class="inline-block medidor relative">						
+		  				<span id="w_disk_io-top" class="top"></span>
+		  				<span id="w_disk_io-bot" class="bot"></span>
 					</div>
-	  		</td>
-        <td class="block frame inf-frame" colspan="2">
-          <div>
-            <p align="center" class="inf-title left-marging-300">BD ACTIVIDAD ACTUAL</p>
-            <div align="center" class="container-info-canvas">
-              <textarea name="inf_bd_conn" id="inf_bd_conn" class="info-canvas data-list-l"></textarea>
+					<canvas id="disk_io" class="inline-block"></canvas>
+				</td>
+				<td class="section">
+					<div class="block section-title">
+						<div class="title">RAM - CONSUMO</div>
+						<div class="section-general-data">
+							<span id="ram_act"></span>
+						</div>
+					</div>				
+					<div class="inline-block medidor relative">
+						<span id="ram-top" class="top"></span>
+		  				<span id="ram-bot" class="bot"></span>
+					</div>
+					<canvas id="ram" class="inline-block"></canvas>
+				</td>
+				<td class="section">
+					<div class="block section-title">
+						<div class="title">CPU - RENDIMIENTO</div>
+						<div class="section-general-data">
+							<span id="cpu_act"></span>
+						</div>
+					</div>			
+					<div class="inline-block medidor relative">
+						<span id="cpu-top" class="top"></span>
+		  				<span id="cpu-bot" class="bot"></span>
+					</div>
+					<canvas id="cpu" class="inline-block"></canvas>
+				</td>
+			</tr>
+			<tr>
+				<td class="section">
+          <div class="block section-title">
+            <div class="title">RED - CONEXIONES</div>
+            <div class="section-general-data">
+              <span id="net_act"></span>
             </div>
           </div>
-        </td>
+          <div class="inline-block medidor relative">
+            <span id="net-top" class="top"></span>
+              <span id="net-bot" class="bot"></span>
+          </div>
+          <canvas id="server_conn" class="inline-block"></canvas>
+				</td>
+				<td class="section">
+          <div class="block section-title">
+            <div class="title">BD - CONEXIONES</div>
+            <div class="section-general-data">
+              <span id="bd_act"></span>
+            </div>
+          </div>
+          <div class="inline-block medidor relative">
+            <span id="bd-top" class="top"></span>
+              <span id="bd-bot" class="bot"></span>
+          </div>
+          <canvas id="bd_conn" class="inline-block"></canvas>
+				</td>
+				<td class="section">
+					<div class="block section-title">
+						<div class="title">INFORMACION GENERAL - ALERTAS</div>
+						<div class="section-general-data">
+							<span id="tot_alert"></span>
+						</div>
+					</div>
+					<div class="inline-block container-general-report">
+						<table class="table-general-report">
+							<tbody>
+								<tr align="left" id="uptime_data">
+			  						<td>Uptime: </td>
+			  						<td class="table-data" colspan="3"><span id="uptime"></span></td>
+			  					</tr>
+			  					<tr align="left" id="average_data">
+			  						<td>CPU AVERAGE: </td>
+									<td class="table-data" colspan="3"><span id="cpu_load"></span></td>
+			  					</tr>
+			  					<tr align="left">
+			  						<td>Uso de memoria APACHE: </td>
+			  						<td class="table-data" colspan="3"><span id="usage_ram"></span></td>
+			  					</tr>
+			  					<tr align="left" id="hdd_data">
+			 						  <td>Uso HDD: </td>
+			 						  <td class="table-data-alerts"><span id="usage_hdd"></span></td>
+                    <td>MAX W-R: </td>
+                    <td class="table-data-min"><span id="max_disk">0</span></td>
+			  					</tr>
+			  					<tr align="left">
+			  						<td>Alertas CPU: </td>
+			  						<td class="table-data-alerts"><span id="alert_cpu">0</span></td>
+                    <td>MAX CPU: </td>
+                    <td class="table-data-min"><span id="max_cpu">0</span></td>
+			  					</tr>
+			  					<tr align="left">
+			  						<td>Alertas RAM: </td>
+			  						<td class="table-data-alerts"><span id="alert_ram">0</span></td>
+                    <td>Max RAM: </td>
+                    <td class="table-data-min"><span id="max_ram">0</span></td>
+			  					</tr>
+			  					<tr align="left">
+                    <td>Alertas NET: </td>
+                    <td class="table-data-alerts"><span id="alert_net">0</span></td>
+			  						<td>Maximo net: </td>
+			  						<td class="table-data-min"><span id="max_http">0</span></td>
+			  					</tr>
+			  					<tr align="left">
+                    <td>Alertas BD: </td>
+                    <td class="table-data-alerts"><span id="alert_bd">0</span></td>
+			  						<td>Maximo bd: </td>
+			  						<td class="table-data-min"><span id="max_bd">0</span></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</td>
+			<tr>
+			<tr>
+				<td class="section">
+					<div class="block section-title">
+						<div class="title">LISTA DE CONEXIONES ACTUALES EN RED</div>
+						<div class="section-general-data">
+							<span id="tot_net"></span>
+						</div>
+					</div>
+					<div class="container-data-area">
+						<table class="table-general-report">
+							<tbody id="inf_net_conn"></tbody>
+						</table>
+					</div>
+				</td>
+				<td class="section" colspan="2">
+					<div class="block section-title">
+						<div class="title">LISTA DE PROCESOS ACTIVOS EN BASE DE DATOS</div>
+					</div>
+					<div class="container-data-area">
+						<table class="table-general-report">
+							<tbody id="inf_bd_conn"></tbody>
+						</table>
+					</div>
+				</td>
 			</tr>
 		</tbody>
 	</table>
-    <script src="js/jquery.min.js"></script>
+
+	<script src="js/jquery.min.js"></script>
     <script src="js/index.js"></script>
   </body>
 </html>
